e2ea35d242fde0dcebdbc3b842bba7ba8a173f09
dsyph3r <dsyph3r@gmail.com>
2011-02-10 22:53:59 +0000

    very basic js interactions

--- mobile/application/js/medo.js
+++ mobile/application/js/medo.js
@@ -1,6 +1,7 @@
 
 function MeDo() {
   
+  this.todoList = [];
   this._init();
   
 }
@@ -9,8 +10,67 @@ MeDo.prototype._init = function() {
   
 }
 
+MeDo.prototype.loadRaw = function(raw) {
+  
+  for (var i = 0; i < raw.length; i++) {
+    this.add(raw[i]);
+  }
+  
+}
+
+MeDo.prototype.add = function(todo) {
+  
+  // Add to the list
+  this.todoList.push(todo);
+  
+  // Update the screen
+  this.render();
+  
+}
+
+MeDo.prototype.remove = function(index) {
+  
+  // Remove from the list
+  this.todoList.splice(index, 1);
+  
+  // Update the screen
+  this.render();
+}
+
+
+MeDo.prototype.render = function() {
+
+  /** Going to lazy render for now, just redraw the whole list **/
+  
+  // Clean list
+  $('#todo ul').html("");
+  
+  // Add items
+  for (var i = 0; i < this.todoList.length; i++) {
+    var htmlClass = "";
+    if (0 == i)
+      htmlClass += "first";
+    
+    if ((this.todoList.length-1) == i || (1 == this.todoList.length))
+      htmlClass += " last";
+      
+    var html = '<li class="' + htmlClass + '"><a onclick="medo.remove(' + i + ')" href="#">' + this.todoList[i] + '</a></li>';
+    $('#todo ul').append(html)
+  }
+  
+  
+}
+
 MeDo.prototype.addNew = function() {
-  alert("TODO: addNew");
+  
+  var random = [
+    "Buy cat food",
+    "Build some more useful phone apps",
+    "Sleep"
+  ];
+
+  this.add(random[Math.floor(Math.random()* (random.length))])
+
 }
 
 MeDo.prototype.exit = function() {
