b3bea9fb68d617ae239cdf7ded410f6e645a1ba3
dsyph3r <dsyph3r@gmail.com>
2011-02-10 21:50:57 +0000

    created android project, setup phonegap. Built simple html layout with static data

---
+++ mobile/application/js/medo.js
@@ -0,0 +1,18 @@
+
+function MeDo() {
+  
+  this._init();
+  
+}
+
+MeDo.prototype._init = function() {
+  
+}
+
+MeDo.prototype.addNew = function() {
+  alert("TODO: addNew");
+}
+
+MeDo.prototype.exit = function() {
+  alert("TODO: exit");
+}
